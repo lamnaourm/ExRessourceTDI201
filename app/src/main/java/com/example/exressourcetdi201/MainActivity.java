127c657d388f21ef0f8b2c3cc21de7bdfe0dfa63
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-05 14:03:02 +0000

    Exemple de declaration d'une ressource de type chaine

--- app/src/main/java/com/example/exressourcetdi201/MainActivity.java
+++ app/src/main/java/com/example/exressourcetdi201/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Resuperer la valeur de chaine2
+        String ch = getString(R.string.chaine2);
+
+
     }
 }
