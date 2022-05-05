5871ebeb595235ab942233576ae68a413abdc4fb
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-05 14:54:24 +0000

    Les couleurs + Les dimensions

--- app/src/main/java/com/example/exressourcetdi201/MainActivity.java
+++ app/src/main/java/com/example/exressourcetdi201/MainActivity.java
@@ -8,12 +8,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button t;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btn = findViewById(R.id.btn);
 
         // Resuperer la valeur de chaine2
         String ch = getString(R.string.chaine2);
@@ -28,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
         // Recuperer un tab des entiers
         int[] b = getResources().getIntArray(R.array.taux);
 
+        // Recuperer couleur
+        btn.setBackgroundColor(getResources().getColor(R.color.c2));
+
+        // Recuperer dimension
+        btn.setTextSize(getResources().getDimension(R.dimen.d2));
 
     }
 }
